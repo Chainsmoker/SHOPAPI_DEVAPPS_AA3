bbc5f3ef8e101c967053fe6a3891e70a2243d214
Alejandro <75154124+Chainsmoker@users.noreply.github.com>
2024-06-14 07:42:11 -0500

    Update admin.py

--- apps/customers/admin.py
+++ apps/customers/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import Customer
 
 admin.site.register(Customer)
+
+admin.site.site_header = 'E-commerce Admin'
+admin.site.site_title = 'E-commerce Admin Portal'
+admin.site.index_title = 'E-commerce Admin Portal'
